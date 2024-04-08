22c976c82645dca9397781b3110cf59ef7e60e89
Rohit Gajula <rohitgajula27@gmail.com>
2024-04-08 20:03:12 +0530

    permission updates

--- billing_project/settings.py
+++ billing_project/settings.py
@@ -142,8 +142,12 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.BasicAuthentication',
-    )
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
 }
 
 SIMPLE_JWT = {
